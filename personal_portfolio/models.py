3cc962cb1953c52f49d512c3775af5230dc01346
vaniakazeko <vania.kazeko.00@bk.ru>
2022-08-13 23:39:31 +0300

    final fix

---
+++ personal_portfolio/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    img = models.ImageField(upload_to='personal_portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
